6e4502a62d786b34aee9196ca92ce83ea6d08fa2
Onur Abdulaji <onurabdulaji@gmail.com>
2023-06-07 23:49:39 +0200

    #Entites Added

---
+++ ApiConsume/HotelProject.EntityLayer/Concrete/Staff.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.EntityLayer.Concrete
+{
+    public class Staff
+    {
+        public int StaffID { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string SociaalMedia1 { get; set; }
+        public string SociaalMedia2 { get; set; }
+        public string SociaalMedia3 { get; set; }
+    }
+}
