33cf0668b897a791d77e66e6cec5e959cb58973c
Onur Abdulaji <onurabdulaji@gmail.com>
2023-06-09 04:29:06 +0200

    ## Personal Update Added ## Personal Delete Added ## New Personal Create Added

--- Frontend/HotelProject.WebUI/Controllers/StaffController.cs
+++ Frontend/HotelProject.WebUI/Controllers/StaffController.cs
@@ -41,8 +41,44 @@ namespace HotelProject.WebUI.Controllers
             var client = _httpClientFactory.CreateClient();
             var jsonData = JsonConvert.SerializeObject(model);
             StringContent stringContent = new StringContent(jsonData,Encoding.UTF8,"application/json");
-            var respondeMessage = await client.PostAsync("https://localhost:44345/api/Staff",stringContent);
-            if (respondeMessage.IsSuccessStatusCode)
+            var responseMessage = await client.PostAsync("https://localhost:44345/api/Staff",stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public async Task <IActionResult> DeleteStaff(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:44345/api/Staff/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateStaff(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:44345/api/Staff/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateStaffViewModel>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateStaff(UpdateStaffViewModel model)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(model);
+            StringContent stringContent = new StringContent(jsonData,Encoding.UTF8,"application/json");
+            var responseMessage = await client.PutAsync("https://localhost:44345/api/Staff/",stringContent);
+            if (responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index");
             }
---
+++ Frontend/HotelProject.WebUI/Models/Staff/UpdateStaffViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelProject.WebUI.Models.Staff
+{
+    public class UpdateStaffViewModel
+    {
+        public int StaffID { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string SociaalMedia1 { get; set; }
+        public string SociaalMedia2 { get; set; }
+        public string SociaalMedia3 { get; set; }
+    }
+}
