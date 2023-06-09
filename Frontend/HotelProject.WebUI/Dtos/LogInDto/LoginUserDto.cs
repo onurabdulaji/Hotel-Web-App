2df282eaf395631da10a6e9af87b799f607b9950
Onur Abdulaji <onurabdulaji@gmail.com>
2023-06-09 09:10:15 +0200

    # Register Dto # Register Process # Login Process
    # All Of Them Added

---
+++ Frontend/HotelProject.WebUI/Dtos/LogInDto/LoginUserDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.LogInDto
+{
+    public class LoginUserDto
+    {
+        [Required(ErrorMessage = "Kullanici Adi Giriniz")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Sifre Giriniz")]
+        public string Password { get; set; }
+    }
+}
---
+++ Frontend/HotelProject.WebUI/Dtos/RegisterDto/CreateNewUserDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.RegisterDto
+{
+    public class CreateNewUserDto
+    {
+        [Required(ErrorMessage ="Ad Alani Gereklidir")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Soyad Alani Gereklidir")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Kullanici Adi Alani Gereklidir")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Mail Alani Gereklidir")]
+        public string Mail { get; set; }
+
+        [Required(ErrorMessage = "Sifre Alani Gereklidir")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Sifre Tekrar Alani Gereklidir")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
