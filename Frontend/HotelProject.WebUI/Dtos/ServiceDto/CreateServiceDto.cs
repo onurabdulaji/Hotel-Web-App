ba285f207c08eeb32b2e3975b0f1babc8cd18e28
Onur Abdulaji <onurabdulaji@gmail.com>
2023-06-09 08:25:17 +0200

    # Identity Lib Added

---
+++ Frontend/HotelProject.WebUI/Dtos/ServiceDto/CreateServiceDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.ServiceDto
+{
+    public class CreateServiceDto
+    {
+        [Required(ErrorMessage = "Hizmet Ikon Linki Giriniz")]
+        public string ServiceIcon { get; set; }
+        [Required(ErrorMessage = "Hizmet Basligi  Giriniz")]
+        [StringLength(100,ErrorMessage ="Hizmet basligi en fazla 100 karakter olabilir.")]
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Frontend/HotelProject.WebUI/Dtos/ServiceDto/UpdateServiceDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.ServiceDto
+{
+    public class UpdateServiceDto
+    {
+        public int ServiceID { get; set; }
+        [Required(ErrorMessage = "Hizmet Ikon Linki Giriniz")]
+        public string ServiceIcon { get; set; }
+        [Required(ErrorMessage = "Hizmet Basligi  Giriniz")]
+        [StringLength(100, ErrorMessage = "Hizmet basligi en fazla 100 karakter olabilir.")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Hizmet Aciklamasi  Giriniz")]
+        [StringLength(500, ErrorMessage = "Hizmet aciklamasi en fazla 500 karakter olabilir.")]
+        public string Description { get; set; }
+    }
+}
