97ff3ca229c4d0ea4092613924d872db27c743d1
Onur Abdulaji <onurabdulaji@gmail.com>
2023-06-10 03:16:44 +0200

    ## User Theme Added ## VIew Component Added

--- ApiConsume/HotelProject.WebApi/Startup.cs
+++ ApiConsume/HotelProject.WebApi/Startup.cs
@@ -48,6 +48,10 @@ namespace HotelProject.WebApi
             services.AddScoped<ITestimonialDal, EfTestimonialDal>();
             services.AddScoped<ITestimonialService, TestimonialManager>();
 
+            services.AddScoped<IAboutDal, EfAboutDal>();
+            services.AddScoped<IAboutService, AboutManager>();
+
+
             services.AddAutoMapper(typeof(Startup));
 
             //Api Cors
